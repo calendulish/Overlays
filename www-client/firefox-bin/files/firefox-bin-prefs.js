8a2a193b77fec59618deed359265abbc7b94d980
Lara Maia <lara@craft.net.br>
2014-05-27 10:26:31 -0300

    Add firefox-bin ebuild

---
+++ www-client/firefox-bin/files/firefox-bin-prefs.js
@@ -0,0 +1,23 @@
+pref("app.update.enabled",                      false);
+pref("app.update.autoInstallEnabled",           false);
+pref("browser.display.use_system_colors",       true);
+pref("browser.shell.checkDefaultBrowser",       false);
+pref("browser.EULA.override",                   true);
+pref("intl.locale.matchOS",                     true);
+
+pref("browser.fullscreen.animateUp",            0);
+pref("browser.download.animateNotifications",   false);
+pref("browser.panorama.animate_zoom",           false);
+pref("browser.preferences.animateFadeIn",       false);
+pref("browser.tabs.animate",                    false);
+
+pref("browser.tabs.closeButton",                0);
+pref("browser.link.open_newwindow.restriction", 0);
+pref("security.dialog_enable_delay",            0);
+pref("browser.sessionhistory.max_entries",      10);
+
+pref("network.http.pipelining",                 true);
+pref("network.proxy.pipelining",                true);
+pref("network.http.pipelining.maxrequrests",    8);
+pref("network.http.max-connections",            96);
+pref("network.http.max-connections-per-server", 32);
